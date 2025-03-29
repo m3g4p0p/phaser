79309ee0178b716753f9cd4b76bd7a7248a84b95
Sebastian Seitz <bassistsagainstracists@gmx.net>
2025-03-29 16:03:05 +0100

    add game scene

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import kaplay from "kaplay"
 // import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
-const k = kaplay()
+const k = kaplay({ width: 400, height: 800, background: "#000000" })
 
 k.loadRoot("./") // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png")
+k.loadSprite("player", "sprites/playerShip1_blue.png")
 
-k.add([k.pos(120, 80), k.sprite("bean")])
+k.scene("game", () => {
+  k.add([
+    k.pos(k.width() / 2, k.height() / 2),
+    k.sprite("player"),
+    k.anchor("center"),
+  ])
+})
 
-k.onClick(() => k.addKaboom(k.mousePos()))
+k.go("game")
